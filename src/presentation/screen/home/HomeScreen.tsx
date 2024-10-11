f080cba0762b6ca54e146104decbbde71398f501
martinrdrz <martinrdrz@hotmail.com>
2024-10-11 17:29:10 -0300

    app finalizada

--- src/presentation/screen/home/HomeScreen.tsx
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -1,43 +1,49 @@
 import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-paper';
-import {getPokemons} from '../../../actions/pokemons/get-pokemons';
-import {useInfiniteQuery} from '@tanstack/react-query';
+import {FAB, Text, useTheme} from 'react-native-paper';
+
+import {useInfiniteQuery, useQueryClient} from '@tanstack/react-query';
 import {PokeballBg} from '../../components/ui/PokeballBg';
 import {FlatList} from 'react-native-gesture-handler';
-import React from 'react';
 import {globalTheme} from '../../../config/theme/global-theme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {PokemonCard} from '../../components/pokemons/PokemonCard';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../../navigator/StackNavigator';
+import {getPokemons} from '../../../actions/pokemons/get-pokemons';
 
-export const HomeScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'HomeScreen'> {}
+
+export const HomeScreen = ({navigation}: Props) => {
   const {top} = useSafeAreaInsets();
+  const queryClient = useQueryClient();
+  const theme = useTheme();
 
-  // const {isLoading, data: pokemons = []} = useQuery({
+  //* Esta es la forma tradicional de una petición http
+  // const {isLoading, data: pokemons = [] } = useQuery({
   //   queryKey: ['pokemons'],
   //   queryFn: () => getPokemons(0),
-  //   staleTime: 1000 * 60 * 60,
+  //   staleTime: 1000 * 60 * 60, // 60 minutes
   // });
 
   const {isLoading, data, fetchNextPage} = useInfiniteQuery({
     queryKey: ['pokemons', 'infinite'],
     initialPageParam: 0,
     staleTime: 1000 * 60 * 60, // 60 minutes
-    queryFn: params => getPokemons(params.pageParam),
-    getNextPageParam: (lastPage, pages) => pages.length,
-    // queryFn: async params => {
-    //   const pokemons = await getPokemons(params.pageParam);
-    //   pokemons.forEach(pokemon => {
-    //     QueryClient.setQueryData(['pokemon', pokemon.id], pokemon);
-    //  });
+    queryFn: async params => {
+      const pokemons = await getPokemons(params.pageParam);
+      pokemons.forEach(pokemon => {
+        queryClient.setQueryData(['pokemon', pokemon.id], pokemon);
+      });
 
-    //   return pokemons;
-    // },
-    // getNextPageParam: (lastPage, pages) => pages.length,
+      return pokemons;
+    },
+    getNextPageParam: (lastPage, pages) => pages.length,
   });
 
   return (
     <View style={globalTheme.globalMargin}>
       <PokeballBg style={styles.imgPosition} />
+
       <FlatList
         data={data?.pages.flat() ?? []}
         keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
@@ -45,9 +51,17 @@ export const HomeScreen = () => {
         style={{paddingTop: top + 20}}
         ListHeaderComponent={() => <Text variant="displayMedium">Pokédex</Text>}
         renderItem={({item}) => <PokemonCard pokemon={item} />}
-        onEndReachedThreshold={0.5}
+        onEndReachedThreshold={0.6}
         onEndReached={() => fetchNextPage()}
         showsVerticalScrollIndicator={false}
+      />
+
+      <FAB
+        label="Buscar"
+        style={[globalTheme.fab, {backgroundColor: theme.colors.primary}]}
+        mode="elevated"
+        color={theme.dark ? 'black' : 'white'}
+        onPress={() => navigation.push('SearchScreen')}
       />
     </View>
   );
